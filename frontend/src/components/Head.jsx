d788d3e33f7f363e47d4374f0adc781c05f4fe68
Joy Baek <juwoo100@gmail.com>
2025-10-22 04:21:12 -0400

    Modified Head.jsx

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -4,8 +4,8 @@ export default function Head() {
             <nav className="container mx-auto flex justify-between items-center px-6 py-3">
                 <h3 className="text-2xl font-bold text-gray-800">IT SERVICE TRACKER</h3>
                 <div className="space-x-4 text-gray-700 font-medium">
-                    <a href="/dashboard" className="hover:text-amber-600 transition">Dashboard | </a>
-                    <a href="/new" className="hover:text-amber-600 transition">Add Ticket | </a>
+                    <a href="/dashboard" className="hover:text-amber-600 transition">Dashboard</a>
+                    <a href="/new" className="hover:text-amber-600 transition">Add Ticket</a>
                     <a href="/login" className="hover:text-amber-600 transition">Login/Sign up</a>
                 </div>
             </nav>
