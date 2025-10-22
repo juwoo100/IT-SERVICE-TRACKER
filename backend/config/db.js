318834a5e4cdff404cc177449eb9520033b1730f
Joy Baek <juwoo100@gmail.com>
2025-10-22 04:29:58 -0400

    Built MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            'process.env.MONGO_URI',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+        console.log('MongoDB connected');
+
+    }catch(err) {
+        console.error('MogoDB connection failed: ', err.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
